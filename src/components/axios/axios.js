e3bab245e018e661d1981448adcf297d3bb1b64b
Alexey <a.lapin03@gmail.com>
2023-02-01 22:26:52 +0300

    add validation, errors check in SignUpPage, update axios instance

--- src/components/axios/axios.js
+++ src/components/axios/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1/'
+  baseURL: 'http://localhost:8080/api/v1/',
+  headers: {
+    'Access-Control-Allow-Origin':'http://localhost:8080/api/v1/auth/register'
+  }
 })
 export default instance
--- src/components/pages/SignUpPage.jsx
+++ src/components/pages/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import {useDispatch} from 'react-redux'
@@ -11,12 +11,31 @@ import {instance as _axios} from '../axios/axios'
 import User from '../../entities/User'
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 import { validate } from '../../utils/validators/UserValidate.ts'
+import Dialog from 'react-toolbox/lib/dialog'
+import { setUser } from '../../store/slices/UserSlice.js'
 
 
 const SignUpPage = () => {
   const [state, setState] = useState({username: '', password: '', error_username: '', error_password: ''})
+  const [dialog, setDialog] = useState({active: false, msg: ''})
+  const [active, setActive] = useState(false)
+  const handleDialog = (name, value) => {
+    setDialog(() => (prevState => ({
+      ...prevState,
+      [name] : value
+    })))
+    console.log(dialog)
+  }
+  useEffect(() => {
+    console.log(active)
+  }, [active])
+
+  const actions = [
+    { label: 'Close', onClick: () => setDialog({active: false, msg: ''})},
+  ]
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const username_hit = 'at least eight characters\n no special character\n no whitespace allowed'
 
@@ -40,6 +59,7 @@ const SignUpPage = () => {
   }
 
   const handleOnSubmit = () => {
+    console.log(state)
     _axios({
       method: 'post',
       url: 'auth/register',
@@ -49,13 +69,14 @@ const SignUpPage = () => {
       },
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin:' : '*'
       }
     }).then( (response) => {
-      dispatch(setUser(new User(username, password, response.data.token)))
+      console.log(response)
+      dispatch(setUser(new User(state.username, state.password, response.data.token)))
       navigate('/main', {replace: true})
     }).catch( (error) => {
       console.log(error)
+      setDialog(() => ({active: true, msg: error.message}))
     })
   }
 
@@ -73,11 +94,19 @@ const SignUpPage = () => {
             required
             onChange={handleChange.bind(this,'username')}
             maxLength={16 } error={state.error_username}
-            hit={username_hit} />
+            hit='some hit' />
           <Input type='password' label='Password' icon='password' name='name' value={state.password} required onChange={handleChange.bind(this,'password')} maxLength={32 } error={state.error_password}/>
           <Button icon='login' label='Submit' onClick={handleOnSubmit} raised primary disabled={false} />
         </CardText>
       </Card>
+      <Dialog
+        actions={actions}
+        active={dialog.active}
+        onEscKeyDown={() => setDialog({active: false, msg: ''})}
+        onOverlayClick={() => setDialog({active: false, msg: ''})}
+        title={dialog.msg}
+      >
+      </Dialog>
     </section>
   )
 }
